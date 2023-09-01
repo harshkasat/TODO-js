4ee06556e7a80d5a7939908aff18f0e4913280fb
Harsh Kasat <hkasat@waymore.io>
2023-09-01 23:46:13 +0530

    Adding all file once

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const input_box = document.getElementById("input_box");
+const list_container = document.getElementById("list_container");
+
+
+function addTask() {
+    if(input_box.value === ""){
+        alert("Please enter write something")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML =input_box.value;
+        list_container.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    input_box.value = "";
+}
+
+
+list_container.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+
+
+},false);
